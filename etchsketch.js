6a94642b0bebb5924af644fad993442ba0746608
Saleem ayyoub <57151873+Sa1eem@users.noreply.github.com>
2019-11-19 20:12:06 +0200

    Add files via upload

---
+++ etchsketch.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+//populates the page on initial load
+var divs = "";
+for (var i = 1;  i <= 256;  i++) {
+divs += '<div class="squares"></div>';
+};
+$(".wrapper").append(divs);
+$('.squares').mouseenter(function() {
+$(this).addClass("visited");
+});
+
+//changes div color from black to yellow on mouseover
+$('#chooseSize').click(function() {
+populate();
+$('.squares').mouseenter(function() {
+$(this).addClass("visited");
+});
+});
+
+//changes div color to yellow then fades to white on mouseover
+$('#fadeOut').click(function() {
+populate();
+$('.squares').mouseenter(function() {
+$(this).animate({opacity: 0}).addClass("visited");
+});
+});
+
+//changes div color to random hex - found random color function on Stack Overflow
+$('#random').click(function() {
+populate();
+$('.squares').mouseenter(function() {
+var color = '#' + Math.random().toString(16).substring(2, 8);
+$(this).css("background-color", color);
+});
+});
+});
+
+//prompts the user for input and populates a new sketchpad based on input
+function populate() {
+var temp = "";
+var div = '<div class="squares"></div>';
+var input = parseInt(prompt("Please choose the number of squares (min: 2, max: 70):"), 10);
+$('.squares').remove();
+for (var i = 1;  i <= input * input;  i++) {
+temp += div;
+};
+$(".wrapper").append(temp);
+var height = 640 / input;
+$('.squares').css('height', height);
+$('.squares').css('width', height);
+}
